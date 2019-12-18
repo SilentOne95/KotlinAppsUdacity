e6a4aa220dbd7d87bbcc94edad511e5f6a32de6e
SilentOne95 <silentone@onet.pl>
2019-12-18 16:29:37 +0100

    Added new app with starter code - Architecture App

---
+++ architectureapp/src/main/java/com/example/architectureapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.architectureapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Creates an Activity that hosts all of the fragments in the app
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
