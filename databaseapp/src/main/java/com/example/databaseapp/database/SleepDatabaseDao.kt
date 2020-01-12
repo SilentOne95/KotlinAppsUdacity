80bf2315ff3df85a94bf0088c74cae4902605c3b
Marcin <SilentOne@onet.pl>
2020-01-12 23:02:20 +0100

    Created db and test- Database App

--- databaseapp/src/main/java/com/example/databaseapp/database/SleepDatabaseDao.kt
+++ databaseapp/src/main/java/com/example/databaseapp/database/SleepDatabaseDao.kt
@@ -1,3 +1,29 @@
 package com.example.databaseapp.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDatabaseDao {
+
+    @Insert
+    fun instert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun query(key: Long): SleepNight?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+}
