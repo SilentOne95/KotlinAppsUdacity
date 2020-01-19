3a04f106223e45afb398fcc50d75c28f2ec1e491
Marcin <SilentOne@onet.pl>
2020-01-19 01:15:49 +0100

    Added started code - Network App

---
+++ networkapp/src/main/java/com/example/networkapp/detail/DetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.networkapp.detail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.networkapp.network.MarsProperty
+
+class DetailViewModel(@Suppress("UNUSED_PARAMETER") marsProperty: MarsProperty, app: Application) :
+    AndroidViewModel(app) {
+}
---
+++ networkapp/src/main/java/com/example/networkapp/network/MarsApiService.kt
@@ -0,0 +1,4 @@
+package com.example.networkapp.network
+
+private const val BASE_URL = "https://mars.udacity.com/"
+
